6ecd2c700ca6e4e25c40960275d3abc4036613b0
Himanshu Jain <hjain@hjain-mbp.jnpr.net>
2016-07-27 10:52:20 -0700

    structural changes

---
+++ content.js
@@ -0,0 +1,17 @@
+console.log("CONTENTTT");
+$('h3').highlight('highlight');
+/*check if page is saved in storage...
+
+if yes -> get the saved info from that
+
+if no -> do nothing
+
+
+{
+	application: MarkIt,
+	saved_pages:[
+	'page1':'somewaytostore',
+	'page2':'somewaytostore'
+	]
+
+}*/
