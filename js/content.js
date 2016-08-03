47c95819edec76adb566c09832193944a56d0c0d
Himanshu Jain <Himanshu Jain>
2016-08-02 23:14:02 -0700

    minor enhancements

--- js/content.js
+++ js/content.js
@@ -1,11 +1,57 @@
 console.log("Initiating content script");
 
+console.log(window.location.href)
+var keys = [];
+
+window.onkeyup = function(e) {keys[e.keyCode]=false;}
+window.onkeydown = function(e) {keys[e.keyCode]=true;}
+
+
+function isNewPage(){
+	var empty_status = false;
+	chrome.storage.sync.get(window.location.href, function (obj) {
+		empty_status = (Object.keys(obj).length == 0)
+		console.log("inside" + empty_status)
+	});
+	console.log("outside"+empty_status)
+	return empty_status
+}
+
+function loadHighlights(){
+	console.log("loading saved highlights")
+	chrome.storage.sync.get(null, function(items) {
+    var allKeys = Object.keys(items);
+    console.log(allKeys);
+	});
+}
+
+function highlightText(text){
+	$('body').highlight(text);
+	console.log("is it new page: " +isNewPage())
+	if(isNewPage()){
+		console.log("saving new highlight")
+		pageurl = window.location.href;
+		chrome.storage.sync.set({pageurl: [{"text":text, "color":"yellow"}]}, function() {
+          // Notify that we saved.
+          message('highlight saved');
+        });
+	}
+	else{
+		console.log("not new page")
+	}
+}
+
+if(!isNewPage()){
+    loadHighlights();
+}
+
 $('body').mouseup(function(e) {
-    if(e.shiftKey){
-        console.log("shift is pressed")
+    if(keys["72"]){ //h is pressed
+        console.log("h is pressed")
         var text=getSelectedText();
         if (text!=''){
-            $('body').highlight(text);}
+        	highlightText(text);            
+        }
     }
 });
 
@@ -17,21 +63,3 @@ function getSelectedText() {
     }
     return '';
 }
-
-
-
-/*check if page is saved in storage...
-
-if yes -> get the saved info from that
-
-if no -> do nothing
-
-
-{
-	application: MarkIt,
-	saved_pages:[
-	'page1':'somewaytostore',
-	'page2':'somewaytostore'
-	]
-
-}*/
