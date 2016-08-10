fed5cb9ce43ef19e774c2281ef925d1b8e7bda6a
Himanshu Jain <Himanshu Jain>
2016-08-10 00:31:57 -0700

    adding manager

--- js/popup.js
+++ js/popup.js
@@ -7,3 +7,10 @@ function storeSettings(){
      var hotkeyHighlight = document.getElementById("hotkeyHighlight");
      localStorage.setItem("hotkeyHighlight", hotkeyHighlight.value);
     }
+
+document.getElementById("clearButton").addEventListener("click",openManager);
+
+function openManager(){
+	var newURL =  chrome.extension.getURL('management.html');;
+  	chrome.tabs.create({ url: newURL });
+     }
