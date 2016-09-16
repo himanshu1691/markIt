9bef54cc4b0cdcc6c24e96878933c97712433f59
Himanshu Jain <Himanshu Jain>
2016-09-15 23:21:14 -0700

    code cleanup

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,3 @@
-document.getElementById("saveButton").addEventListener("click",storeSettings);
-
-function storeSettings(){
-     var textColor = document.getElementById("textColor");
-     localStorage.setItem("textColor", textColor.value);
-
-     var hotkeyHighlight = document.getElementById("hotkeyHighlight");
-     localStorage.setItem("hotkeyHighlight", hotkeyHighlight.value);
-    }
-
 document.getElementById("clearButton").addEventListener("click",openManager);
 
 function openManager(){
