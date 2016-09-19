37b53243dcfd1233e096373d004a0bf9e7b20085
Himanshu Jain <Himanshu Jain>
2016-09-18 22:38:51 -0700

    bug fixes

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,13 @@
 document.getElementById("settingsButton").addEventListener("click",openSettings);
+document.getElementById("managementButton").addEventListener("click",openManager);
 
 function openSettings(){
 	var newURL =  chrome.extension.getURL('settings.html');
   	chrome.tabs.create({ url: newURL });
      }
 
-document.getElementById("managementButton").addEventListener("click",openManager);
+function openManager(){
+	var newURL =  chrome.extension.getURL('management.html');;
+  	chrome.tabs.create({ url: newURL });
+     }
 
